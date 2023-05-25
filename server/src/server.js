44ca3dbfff857acea6c536735214f3705b4ab8b2
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-05-25 15:07:15 +0400

    Populated destination exoplanet with data

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,17 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listenin' on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listenin' on port ${PORT}`);
+  });
+}
+
+startServer();
