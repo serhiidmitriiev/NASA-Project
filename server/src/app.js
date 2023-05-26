05e41b97497747cca3dd4484a0b66bdeb22e2a4b
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-05-26 15:02:02 +0400

    Created get request for launches

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
@@ -10,7 +13,17 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
