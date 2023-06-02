363abd8b41ad306cfc827d66425026d22e884627
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-06-02 20:45:12 +0400

    Connected to mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:kT26FcR4nwNvhBaE@nasacluster.92vxlc3.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listenin' on port ${PORT}`);
