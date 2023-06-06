29a6cb810e1c99f6e5f2c0a9001773fca3ae50fe
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-06-06 13:40:38 +0400

    Updated test for mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,17 @@
 const http = require("http");
-const mongoose = require("mongoose");
 
 const app = require("./app");
+
+const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:kT26FcR4nwNvhBaE@nasacluster.92vxlc3.mongodb.net/nasa?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listenin' on port ${PORT}`);
---
+++ server/src/services/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:kT26FcR4nwNvhBaE@nasacluster.92vxlc3.mongodb.net/nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
